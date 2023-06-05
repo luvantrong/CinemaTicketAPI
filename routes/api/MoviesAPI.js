7e22726dd5047873261e38c4de428ec48f1fbe82
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-06-05 14:22:31 +0700

    ok

--- routes/api/MoviesAPI.js
+++ routes/api/MoviesAPI.js
@@ -1,30 +1,29 @@
 var express = require("express");
 var router = express.Router();
-const movieController = require('../../components/movies/MovieController');
+const movieController = require("../../components/movies/MovieController");
 
 // http://localhost:3000/api/movie/getAllMovies
 // Lấy danh sách phim
-router.get('/getAllMovies', async function(req, res, next) {
-    try {
-        const listMovies = await movieController.getAllMovies();
-        return res.status(200).json({result: true, movies: listMovies});
-    } catch(err) {
-        return res.status(500).json({result: false, products: null});
-    }
+router.get("/getAllMovies", async function (req, res, next) {
+  try {
+    const listMovies = await movieController.getAllMovies();
+    return res.status(200).json({ result: true, movies: listMovies });
+  } catch (err) {
+    return res.status(500).json({ result: false, movies: null });
+  }
 });
 
 // http://localhost:3000/api/movie/searchMovieByName?name=
 // tìm phim theo tên
-router.get('/searchMovieByName', async (req, res, next) => {
-    try {
-        const {name} = req.query;
-        const movie = await movieController.searchMovieName(name);
-        console.log(name);
-        return res.status(200).json({result: true, movie: movie});
-    } catch (error) {
-        return res.status(500).json({result: false, movie: null});
-    }
+router.get("/searchMovieByName", async (req, res, next) => {
+  try {
+    const { name } = req.query;
+    const movie = await movieController.searchMovieName(name);
+    console.log(name);
+    return res.status(200).json({ result: true, movie: movie });
+  } catch (error) {
+    return res.status(500).json({ result: false, movie: null });
+  }
 });
-
 
 module.exports = router;
